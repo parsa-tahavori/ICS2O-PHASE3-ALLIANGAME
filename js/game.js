1bc4a772899f7e24db62c577f18300d713040a61
parsa-tahavori <113055869+parsa-tahavori@users.noreply.github.com>
2022-11-15 18:51:02 +0000

    /.

--- js/game.js
+++ js/game.js
@@ -13,9 +13,20 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: "arcade",
+      arcade: {
+      debug: true,
+    },
+  },
   // set background color
   backgroundColor: 0x5f6e7a,
-};
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autocenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
 
-const game = new Phaser.Game(config);
-console.log(game);
+const game = new Phaser.Game(config)
+console.log(game)
